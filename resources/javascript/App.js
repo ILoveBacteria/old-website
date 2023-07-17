fbe4c21036f1eeca9501c401f82cd1312f32d7ed
Moein Arabi <moein.mo81@gmail.com>
2023-07-17 09:41:22 +0330

    Add bootstrap and fonts

--- resources/javascript/App.js
+++ resources/javascript/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Home} from "./home/Home";
+import '../scss/styles.scss'
+import * as bootstrap from 'bootstrap'
 
 
 let App = () => {
-    return <Home/>
+    return (
+        <div className="container">
+            <Home/>
+        </div>
+    );
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
--- resources/javascript/home/Hero.js
+++ resources/javascript/home/Hero.js
@@ -1,11 +1,10 @@
 import React from "react";
-import '../../css/app.css'
 
 
 export let Hero = () => {
     return (
         <div>
-            <div className="text-3xl font-bold underline">MohammadMoein Arabi</div>
+            <div>MohammadMoein Arabi</div>
             <div>Computer Engineering student at Shahid Beheshti University</div>
         </div>
     );
