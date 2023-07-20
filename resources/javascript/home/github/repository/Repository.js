cca45ae111a5537aa67269c1f09ff5d4d06b248b
Moein Arabi <moein.mo81@gmail.com>
2023-07-20 19:55:24 +0330

    Improve Repository colors

--- resources/javascript/home/github/repository/Repository.js
+++ resources/javascript/home/github/repository/Repository.js
@@ -4,14 +4,14 @@ import '/resources/scss/styles.scss'
 
 export let Repository = () => {
     const style = {
-        backgroundColor: '#2b3137',
+        '--bs-border-color': 'rgb(68, 76, 86)',
         width: '30rem',
     }
 
     return (
-        <div className="p-3 rounded-4 mb-3" style={style}>
-            <div className="fw-semibold fs-4" style={{color: 'rgba(250,251,252,0.7)'}}>Primary card title</div>
-            <div style={{color: 'rgba(250,251,252,0.6)'}}>
+        <div className="p-3 rounded-4 mb-3 border border-1" style={style}>
+            <div className="fw-semibold fs-4" style={{color: 'rgb(173, 186, 199)'}}>Primary card title</div>
+            <div style={{color: 'rgb(118, 131, 144)'}}>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
             </div>
