1ac12a5f5f0dc8f836d493982b72e2c16859e84c
Moein Arabi <moein.mo81@gmail.com>
2023-08-07 22:51:08 +0330

    Add tags

---
+++ resources/javascript/utils/tags.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export let H2 = (props) => {
+    return <h2 className="fw-bold mb-3" style={{color: '#00ADB5'}}>{props.children}</h2>
+}
+
+export let H3 = (props) => {
+    return <h3 className="fw-semibold mb-3" style={{color: '#00ADB5'}}>{props.children}</h3>
+}
+
+export let P = (props) => {
+    return <p style={{maxWidth: '70ch'}}>{props.children}</p>
+}
