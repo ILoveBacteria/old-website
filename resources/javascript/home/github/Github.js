835970022bc0217e246e55cb870e935fc6f1a44a
Moein Arabi <moein.mo81@gmail.com>
2023-08-09 11:07:09 +0330

    Add github title

--- resources/javascript/home/github/Github.js
+++ resources/javascript/home/github/Github.js
@@ -2,17 +2,28 @@ import React from "react";
 import {Repository} from "./repository/Repository";
 import '/resources/scss/styles.scss'
 import {Info} from "./info/Info";
+import {P} from "../../utils/tags";
 
 
 export let Github = () => {
     let style = {
         backgroundColor: '#24292e',
     }
+    const githubStyle = {
+        background: 'linear-gradient(to right, #6f86d6, #48c6ef)',
+        fontSize: '4rem',
+        '-webkit-background-clip': 'text',
+        '-webkit-text-fill-color': 'transparent',
+    }
 
     return (
         <section>
-            <div style={style} className="d-flex justify-content-around px-4 pt-4">
+            <div style={style} className="d-flex justify-content-around align-items-center px-4 pt-4">
                 <div>
+                    <h2 className="fw-bold" style={githubStyle}>GitHub</h2>
+                    <p className="mb-4" style={{color: 'rgb(173, 186, 199)'}}>
+                        Here you can find my latest open source projects.
+                    </p>
                     <Info/>
                 </div>
                 <div>
