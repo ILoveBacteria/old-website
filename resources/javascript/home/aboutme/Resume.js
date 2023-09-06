8cc8db5247da850000776b131bf868d45d970a0c
Moein Arabi <moein.mo81@gmail.com>
2023-09-06 19:59:48 +0330

    Add company logo

--- resources/javascript/home/aboutme/Resume.js
+++ resources/javascript/home/aboutme/Resume.js
@@ -7,11 +7,15 @@ export let Resume = (props) => {
         return placeholder;
     }
     return (
-        <div className="mb-3">
-            <div className="fw-semibold mb-1">{props.data.title}</div>
-            <div>{props.data.subtitle}</div>
-            <div className="text-muted mb-2" style={{fontSize: '0.9rem'}}>{props.data.date}</div>
-            <div>{props.data.skills}</div>
+        <div className="mb-3 d-flex">
+            <img src={props.data.company.logo} style={{width: '2.7rem', height: '2.7rem'}}
+                 className="me-3" alt="Company Logo"/>
+            <div>
+                <div className="fw-semibold mb-1">{props.data.title}</div>
+                <div>{props.data.subtitle}</div>
+                <div className="text-muted mb-2" style={{fontSize: '0.9rem'}}>{props.data.date}</div>
+                <div>{props.data.skills}</div>
+            </div>
         </div>
     );
 }
