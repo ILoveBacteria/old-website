378c461712b0b586d4aef6040e1f0951f9df8325
Moein Arabi <moein.mo81@gmail.com>
2023-09-09 10:03:44 +0330

    Fix backdrop filter

--- resources/javascript/home/Header.js
+++ resources/javascript/home/Header.js
@@ -6,6 +6,7 @@ export let Header = () => {
     let style = {
         backgroundColor: "rgba(255,255,255,0.1)",
         backdropFilter: "blur(10px)",
+        "-webkit-backdrop-filter": "blur(10px)",
     }
 
     return (
