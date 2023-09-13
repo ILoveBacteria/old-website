6702c18597a21f828de0bcced18b9fb9ccd427aa
Moein Arabi <moein.mo81@gmail.com>
2023-09-13 23:39:36 +0330

    Add sync badge with LinkedIn

--- resources/javascript/home/aboutme/AboutMe.js
+++ resources/javascript/home/aboutme/AboutMe.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {H2, H3, P} from "../../utils/tags";
+import {H2WithNoMargin, H3, P} from "../../utils/tags";
 import {SERVER_ADDR} from "../../App";
 import {ResumeList} from "./ResumeList";
 
@@ -28,7 +28,12 @@ export class AboutMe extends React.Component {
     render() {
         return (
             <section id="about" className="mb-5 container">
-                <H2>About Me</H2>
+                <div className="d-flex align-items-center mb-3">
+                    <div className="me-2"><H2WithNoMargin>About Me</H2WithNoMargin></div>
+                    <span className="badge rounded-pill text-bg-warning">
+                        {arrowRepeat} Last sync with LinkedIn: Never
+                    </span>
+                </div>
                 <P>
                     I am a computer engineering student at Shahid Beheshti University. I am interested in
                     Back-end and Front-end development. currently improving myself in programming. I like to experience
@@ -44,3 +49,13 @@ export class AboutMe extends React.Component {
         );
     }
 }
+
+const arrowRepeat = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+         className="bi bi-arrow-repeat" viewBox="0 0 16 16">
+        <path
+            d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
+        <path fillRule="evenodd"
+              d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
+    </svg>
+);
--- resources/javascript/utils/tags.js
+++ resources/javascript/utils/tags.js
@@ -5,6 +5,10 @@ export let H2 = (props) => {
     return <h2 className="fw-bold mb-3" style={{color: '#00ADB5'}}>{props.children}</h2>
 }
 
+export let H2WithNoMargin = (props) => {
+    return <h2 className="fw-bold m-0" style={{color: '#00ADB5'}}>{props.children}</h2>
+}
+
 export let H3 = (props) => {
     return <h3 className="fw-semibold mb-3" style={{color: '#00ADB5'}}>{props.children}</h3>
 }
